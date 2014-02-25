9e0d3eb3dedcf079e4567e665352120744682ad1
Thomas Blazek <wtblazek@gmail.com>
2014-02-25 16:33:18 +0100

    lala

--- +channel/jakes.m
+++ +channel/jakes.m
@@ -12,11 +12,11 @@ delta       = 1; % Steps in Hz of the jakes filter
 fft_size    = 2^nextpow2(f_d*2); % Minimum FFT size to cover the jakes spectrum
 nu          = -f_d+1:delta:f_d-1; % Frequency used (avoid singularities by going to f_d-1
 H           = zeros(fft_size, 1); % Fill up H with zeros outside of jakes band
-H((fft_size/2)-floor(412/delta):fft_size/2+floor(412/delta)) =...
-                                    sqrt(1/pi/f_d./sqrt(1-(nu/f_d).^2));
+%H((fft_size/2)-floor(412/delta):fft_size/2+floor(412/delta)) =...
+ %                                   sqrt(1/pi/f_d./sqrt(1-(nu/f_d).^2));
                                 %sqrt because PSD(h) = N_0*|H|^2, therefore
                                 % |H|^2 has to show jakes form
-                                
+H(1:825)   = sqrt(1/pi/f_d./sqrt(1-(nu/f_d).^2));   
 h = sqrt(fft_size)*ifft(H, fft_size); %unitary FFT
 %h(end/2:end) = 0; %Avoid periodic behaviour
 noise_colored = conv(noise_white, h);
@@ -26,7 +26,7 @@ noise_colored_unity = noise_colored*sqrt(length(noise_colored))./...
 % At this point, the time base is 1/(fft_size*delta*1Hz) per sample. This has to
 % be resampled to achieve 1/(625e3Hz) s per sample. 
 display('start resampling')
-noise_resampled = resample(noise_colored, f_s, floor(f_d*delta/4));
+noise_resampled = resample(noise_colored, f_s, floor(f_d*delta*2));
 t=1:length(noise_colored_unity);
 small_scale_fading_trace =noise_resampled;%noise_colored_unity.'.*exp(1j*pi.*t);% noise_resampled;
 end
---
+++ +channel/jakes_2.m
@@ -0,0 +1,26 @@
+function small_scale_fading_trace = jakes_2()
+N = 10000;
+f_d = 413.001;
+% B1 = [0 32/64-414/625e3];
+% A1 = [0 0];
+% B2 = [32/64-413/625e3:1/625e3:32/64+413/625e3];
+% nu = -413:413;
+% A2 = sqrt(1/pi/f_d./sqrt(1-(nu/f_d).^2));
+% B3 = [32/64+414/625e3 1];
+% A3 = [0 0];
+% F = [B1 B2 B3];
+% A = [A1 A2 A3];
+nu = 0:413;
+B2 = [nu/625e3];
+A2 = sqrt(1/pi/f_d./sqrt(1-(nu/f_d).^2));
+B3 = [414/625e3 1];
+A3 = [0 0];
+A = [A2 A3];
+F = [B2 B3];
+d = fdesign.arbmag('N,F,A', N, F, A);
+Hd = design(d, 'freqsamp', 'systemObject',true);
+noise_white = normrnd(0, 1/sqrt(2), 1, 2000000)+1j*normrnd(0, 1/sqrt(2), 1, 2000000);
+noise_colored = filtfilt(Hd.Numerator,1, noise_white);
+%small_scale_fading_trace = noise_colored(2:2:end).*exp(-1j*pi.*(1:length(noise_colored(2:2:end))));
+small_scale_fading_trace = noise_colored;
+end
